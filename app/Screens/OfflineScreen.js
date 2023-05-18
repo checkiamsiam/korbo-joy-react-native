f46a8c2d34d4b257869dd7b75eb6a6bcde4cf725
checkiamsiam <issiam02415@gmail.com>
2023-05-18 16:23:50 +0600

    header update , offline page added

---
+++ app/Screens/OfflineScreen.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import FeatherIcon from "react-native-vector-icons/Feather";
+import { COLORS, IMAGES } from "../constants/theme";
+
+const OfflineScreen = ({ onRetry }) => {
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Image
+          style={{
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={IMAGES.logo}
+        />
+      </View>
+      <View style={styles.container}>
+        <FeatherIcon name="wifi-off" size={80} color={COLORS.primary} />
+        <Text style={styles.text}>You are offline</Text>
+        <TouchableOpacity style={styles.button} onPress={onRetry}>
+          <Text style={styles.buttonText}>Retry</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.background,
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 24,
+    color: COLORS.text,
+  },
+  button: {
+    backgroundColor: COLORS.primary,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: COLORS.white,
+  },
+});
+
+export default OfflineScreen;
---
+++ app/components/NetworkConnectivity.js
@@ -0,0 +1,25 @@
+import NetInfo from "@react-native-community/netinfo";
+import React, { useEffect, useState } from "react";
+import "react-native-gesture-handler";
+import OfflineScreen from "../Screens/OfflineScreen";
+
+const NetworkConnectivity = ({ children }) => {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsOnline(state.isConnected);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [isOnline]);
+
+  if (!isOnline) {
+    return <OfflineScreen onRetry={() => setIsOnline(true)} />;
+  }
+  return children;
+};
+
+export default NetworkConnectivity;
